383b90e6353891b57f893099e16bb4d84ad2c9b3
tigerabrodi <tigerabrodi@gmail.com>
2021-09-14 21:26:03 +0200

    feat: add 3 different focus styles, pink, yellow and dark

---
+++ styles/sharedStyles.ts
@@ -0,0 +1,32 @@
+import { css } from "styled-components";
+import { theme } from "./theme";
+
+export const pinkFocusStyles = css`
+  &:focus-visible {
+    outline: 0.2rem ridge #ffbeba;
+    outline-offset: 0.3rem;
+  }
+  &:focus:not(:focus-visible) {
+    outline: none;
+  }
+`;
+
+export const yellowFocusStyles = css`
+  &:focus-visible {
+    outline: 0.2rem ridge ${theme.Yellow};
+    outline-offset: 0.3rem;
+  }
+  &:focus:not(:focus-visible) {
+    outline: none;
+  }
+`;
+
+export const darkFocusStyles = css`
+  &:focus-visible {
+    outline: 0.2rem ridge ${theme.Brown};
+    outline-offset: 0.3rem;
+  }
+  &:focus:not(:focus-visible) {
+    outline: none;
+  }
+`;
