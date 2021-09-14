d432c63c846d9eeaa2f53e99c19be5c6f6c4d3bb
tigerabrodi <tigerabrodi@gmail.com>
2021-09-14 06:41:26 +0200

    feat: add auth hooks

---
+++ hooks/useAuthState.ts
@@ -0,0 +1,27 @@
+import { User, Auth, onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { useLoadingValue } from "@hooks/useLoadingValue";
+
+export const useAuthState = (auth: Auth) => {
+  const {
+    error,
+    loading,
+    setError,
+    setValue,
+    value: user,
+  } = useLoadingValue<User | null, Error>(() => auth.currentUser);
+
+  useEffect(() => {
+    const listener = onAuthStateChanged(auth, setValue, setError);
+
+    return () => {
+      listener();
+    };
+  }, [auth, setError, setValue]);
+
+  return {
+    user,
+    loading,
+    error,
+  };
+};
---
+++ hooks/useUserData.ts
@@ -0,0 +1,30 @@
+import { doc, onSnapshot } from "firebase/firestore";
+import { auth, firebaseDb } from "@lib/firebase";
+import { useEffect, useState } from "react";
+import { useAuthState } from "@hooks/useAuthState";
+import { UserData } from "@lib/types";
+
+type User = {
+  username: string;
+};
+
+export function useUserData(): UserData {
+  const { user } = useAuthState(auth);
+  const [username, setUsername] = useState<string | null>(null);
+
+  useEffect(() => {
+    let unsubscribe;
+
+    if (user) {
+      unsubscribe = onSnapshot(doc(firebaseDb, "users", user.uid), (doc) => {
+        setUsername((doc.data() as User).username);
+      });
+    } else {
+      setUsername(null);
+    }
+
+    return unsubscribe;
+  }, [user]);
+
+  return { user, username };
+}
