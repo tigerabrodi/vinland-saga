923b15f1470b1893ec6681e964c730f3a7ae2b74
tigerabrodi <tigerabrodi@gmail.com>
2021-09-14 21:25:28 +0200

    feat: adjust navigation for tablet and desktop layout

--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -1,21 +1,36 @@
-import { NavButtonLink, NavigationWrapper, NavLink, NavLogo } from "./styles";
+import {
+  RegisterLink,
+  LoginLink,
+  NavigationWrapper,
+  NavLink,
+  NavLogo,
+  NavigationContainer,
+  ButtonLinkWrapper,
+} from "./styles";
 import Link from "next/link";
 
 export const Navigation = () => {
   return (
     <NavigationWrapper>
-      <Link passHref href="/">
-        <NavLink>Home</NavLink>
-      </Link>
-      <Link passHref href="/users">
-        <NavLink>Users</NavLink>
-      </Link>
-      <Link passHref href="/">
-        <NavLogo>VS</NavLogo>
-      </Link>
-      <Link passHref href="/sign-up">
-        <NavButtonLink>Create Account</NavButtonLink>
-      </Link>
+      <NavigationContainer>
+        <Link passHref href="/">
+          <NavLink>Home</NavLink>
+        </Link>
+        <Link passHref href="/users">
+          <NavLink>Users</NavLink>
+        </Link>
+        <Link passHref href="/">
+          <NavLogo>VS</NavLogo>
+        </Link>
+        <ButtonLinkWrapper>
+          <Link passHref href="/sign-in">
+            <LoginLink>Login</LoginLink>
+          </Link>
+          <Link passHref href="/sign-up">
+            <RegisterLink>Create Account</RegisterLink>
+          </Link>
+        </ButtonLinkWrapper>
+      </NavigationContainer>
     </NavigationWrapper>
   );
 };
--- components/Navigation/styles.ts
+++ components/Navigation/styles.ts
@@ -1,26 +1,75 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { theme } from "@styles/theme";
 import { pinkFocusStyles, yellowFocusStyles } from "@styles/sharedStyles";
+import { media } from "@styles/media";
 
 export const NavigationWrapper = styled.nav`
   height: 7rem;
-  box-shadow: 0 0.1rem 0.2rem black;
   width: 100%;
-  border-bottom: 0.2rem solid ${theme.Pink};
+  box-shadow: 0 0.2rem 0.2rem black;
+  border-bottom: 0.3rem solid ${theme.Pink};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  ${media.tablet} {
+    height: 8rem;
+  }
+  ${media.desktop} {
+    height: 9rem;
+  }
+`;
+
+export const NavigationContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  height: 100%;
+  width: 100%;
+  ${media.tablet} {
+    width: 95%;
+  }
+  ${media.desktop} {
+    width: 70%;
+  }
+`;
+
+export const ButtonLinkWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-evenly;
 `;
 
-export const NavLink = styled.a`
+const linkStyles = css`
   font-weight: 500;
-  font-size: 1.4rem;
   color: ${theme.Pink};
-  margin-top: 2rem;
   ${pinkFocusStyles}
+  ${media.tablet} {
+    font-size: 1.7rem;
+    margin-top: 1.8rem;
+    transition: all 0.2s ease;
+    &:hover {
+      transform: translateY(-0.2rem);
+      filter: brightness(120%);
+    }
+  }
 `;
 
-export const NavButtonLink = styled.a`
+export const NavLink = styled.a`
+  ${linkStyles}
+  font-size: 1.4rem;
+  margin-top: 2rem;
+`;
+
+export const LoginLink = styled.a`
+  ${linkStyles}
+  display: none;
+  ${media.tablet} {
+    margin-right: 3rem;
+    display: inline;
+  }
+`;
+
+export const RegisterLink = styled.a`
   font-weight: 600;
   font-size: 1.1rem;
   border-radius: 0.2rem;
@@ -30,6 +79,18 @@ export const NavButtonLink = styled.a`
   margin-top: 1rem;
   color: ${theme.Brown};
   ${pinkFocusStyles}
+  ${media.tablet} {
+    font-size: 1.5rem;
+    transition: all 0.2s ease;
+    &:hover {
+      transform: translateY(-0.2rem);
+      box-shadow: 0 0.2rem 0.4rem black;
+    }
+    &:active {
+      transform: translateY(0);
+      box-shadow: 0 0.1rem 0.2rem black;
+    }
+  }
 `;
 
 export const NavLogo = styled.a`
@@ -37,7 +98,22 @@ export const NavLogo = styled.a`
   font-weight: bold;
   font-size: 2.4rem;
   padding: 0.6rem;
-  margin-top: 0.2rem;
+  margin-top: 0.4rem;
+  box-shadow: 0 0.1rem 0.2rem black;
   color: ${theme.Brown};
   ${yellowFocusStyles}
+  ${media.tablet} {
+    font-size: 2.6rem;
+    padding: 0.7rem;
+    box-shadow: 0 0.1rem 0.2rem black;
+    transition: all 0.2s ease;
+    &:hover {
+      transform: translateY(-0.2rem);
+      box-shadow: 0 0.2rem 0.2rem black;
+    }
+    &:active {
+      transform: translateY(0);
+      box-shadow: 0 0.1rem 0.2rem black;
+    }
+  }
 `;
