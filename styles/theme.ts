8a8d718ea49b9ab857e252cb73076dff142d4e7c
tigerabrodi <tigerabrodi@gmail.com>
2021-09-16 06:23:06 +0200

    feat: add dark pink color

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,6 +1,7 @@
 export const theme = {
   Pink: "var(--pink)",
   LightPink: "var(--lightPink)",
+  DarkPink: "var(--darkPink)",
   Brown: "var(--brown)",
   Yellow: "var(--yellow)",
   LightYellow: "var(--lightYellow)",
