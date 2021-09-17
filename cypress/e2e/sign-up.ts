70857a5d27c6403438ea74c1de1cb201bf5853c2
tigerabrodi <tigerabrodi@gmail.com>
2021-09-17 06:36:15 +0200

    test: extend username validation test to also test if username is valid

--- cypress/e2e/sign-up.ts
+++ cypress/e2e/sign-up.ts
@@ -1,9 +1,13 @@
+import { buildUser } from "../support/generate";
+
 beforeEach(() => {
   indexedDB.deleteDatabase("firebaseLocalStorageDb");
   cy.visit("/");
 });
 
-it("Existing usernames can not be used", () => {
+it("Username validation", () => {
+  const user = buildUser();
+
   cy.findByRole("link", { name: "Create Account" }).click();
 
   cy.findByText("Username is already taken.").should("not.exist");
@@ -11,4 +15,10 @@ it("Existing usernames can not be used", () => {
   cy.findByLabelText("Username").type("tigerabrodi");
 
   cy.findByText("Username is already taken.").should("exist");
+
+  cy.findByLabelText("Username").clear();
+
+  cy.findByLabelText("Username").type(user.username);
+
+  cy.findByText("Username is valid.").should("exist");
 });
