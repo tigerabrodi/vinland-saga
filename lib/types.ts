fe29e642de2b5e620c9999a4a21686fe3f05ed81
tigerabrodi <tigerabrodi@gmail.com>
2021-09-19 07:54:33 +0200

    add user profile type

--- lib/types.ts
+++ lib/types.ts
@@ -1,6 +1,22 @@
+import { FieldValue } from "@firebase/firestore";
 import { User } from "firebase/auth";
 
 export type UserData = {
   user: User | null | undefined;
   username: string | null;
+};
+
+export type UserProfile = {
+  username: string;
+  email: string;
+  password: string;
+  fullname: string;
+  age: number;
+  work: string;
+  location: string;
+  biography: string;
+  avatarUrl: string;
+  clapCount: number;
+  recipeCount: number;
+  joined: FieldValue;
 };
