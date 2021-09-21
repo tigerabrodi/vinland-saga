ef2626a7123c295e8cfa563e532f283d8c12dc9e
tigerabrodi <tigerabrodi@gmail.com>
2021-09-21 18:34:41 +0200

    test: assert user profile page and start new recipe

--- cypress/e2e/complete-user-flow.ts
+++ cypress/e2e/complete-user-flow.ts
@@ -10,13 +10,14 @@ it("Complete users flow", () => {
 
   cy.findByRole("link", { name: "Create Account" }).click();
 
+  // Create Account
   cy.findByLabelText("Username").type(user.username);
   cy.findByLabelText("Email").type(user.email);
   cy.findByLabelText("Password").type(user.password);
   cy.findByLabelText("Confirm Password").type(user.password);
-
   cy.findByRole("button", { name: "Sign Up" }).click();
 
+  // Create Profile
   cy.findByText("You successfully created your account.").should("exist");
   cy.findByRole("heading", { name: "Editing Profile" }).should("exist");
   cy.findByRole("button", { name: "Save" }).should("have.attr", "disabled");
@@ -33,8 +34,21 @@ it("Complete users flow", () => {
   cy.findByLabelText("Biography").type(user.bio);
 
   cy.findByRole("button", { name: "Save" }).click();
-
   cy.findByText("Successfully updated your profile.").should("exist");
 
+  // User Profile
   cy.findByRole("heading", { name: user.fullname }).should("exist");
+  cy.findByText(`@${user.username}`).should("exist");
+  cy.findByText(`Age ${user.age}`).should("exist");
+  cy.findByText(`Located in ${user.location}`).should("exist");
+  cy.findByText(user.work).should("exist");
+  cy.findByText(user.bio).should("exist");
+  cy.findByRole("link", { name: "Edit Your Profile" }).should("exist");
+  cy.findByRole("img", { name: user.fullname }).should("exist");
+
+  cy.findByRole("heading", { name: "Recipes" }).should("exist");
+  cy.findByText("You currently have written no recipes.");
+
+  // New Post
+  cy.findByRole("link", { name: "New Recipe" }).click();
 });
