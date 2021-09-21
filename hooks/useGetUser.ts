152e4fcce46cd3c4a4f26b04e9f5d4459dc012ee
tigerabrodi <tigerabrodi@gmail.com>
2021-09-21 18:23:27 +0200

    feat(useGetUser): add hook which returns user's profile given their username

---
+++ hooks/useGetUser.ts
@@ -0,0 +1,24 @@
+import { getUserWithUsername } from "@lib/firebase";
+import { useLoadingStore } from "@lib/store";
+import { UserProfile } from "@lib/types";
+import * as React from "react";
+
+export const useGetUser = (username: string | null) => {
+  const [user, setUser] = React.useState<UserProfile | null>(null);
+  const { setStatus } = useLoadingStore();
+
+  React.useEffect(() => {
+    if (user || !username) {
+      return;
+    }
+
+    const setUserState = async () => {
+      setStatus("loading");
+      setUser((await getUserWithUsername(username)) as UserProfile);
+      setStatus("success");
+    };
+    setUserState();
+  }, [username, setStatus, user]);
+
+  return { user };
+};
