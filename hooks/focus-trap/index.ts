f039492a39bf6a0a1461f5ab6c835546c10ed2ea
tigerabrodi <tigerabrodi@gmail.com>
2021-09-27 07:32:16 +0200

    feat(focus-trap): localize focus trap package and uninstall the previous one
    The previous package contained lodash and also some operations that aren't necessary for our usecase.
    
    Hence for performance reasons, it is better for us to localize the package.
    
    We will also need the package not just for modals, but also other elements such the the button menu in the navigation bar.

---
+++ hooks/focus-trap/index.ts
@@ -0,0 +1,33 @@
+import {
+  getFocusableElements,
+  getKeyDownEventHandler,
+  getDisengageFocusTrapCallback,
+} from "./utils";
+
+export function setUpFocusTrap(
+  trappedElement: HTMLElement
+): (element?: HTMLElement) => void {
+  const lastActiveElement = document.activeElement as HTMLElement;
+  const focusableElements = getFocusableElements(trappedElement);
+  const firstFocusableElement = focusableElements[0];
+  const lastFocusableElement = focusableElements[focusableElements.length - 1];
+  const keyDownEventHandler = getKeyDownEventHandler(
+    firstFocusableElement,
+    lastFocusableElement
+  );
+
+  // Engage focus trap.
+  trappedElement.addEventListener("keydown", keyDownEventHandler);
+
+  if (firstFocusableElement) {
+    firstFocusableElement.focus();
+  }
+
+  // Return a callback that allows to disengage the focus trap
+  // and also restore the focus back to the appropriate element in the page.
+  return getDisengageFocusTrapCallback(
+    trappedElement,
+    keyDownEventHandler,
+    lastActiveElement
+  );
+}
---
+++ hooks/focus-trap/utils.ts
@@ -0,0 +1,70 @@
+export const FOCUSABLE_ELEMENTS = [
+  'input:not([disabled]):not([tabindex="-1"])',
+  'select:not([disabled]):not([tabindex="-1"])',
+  'textarea:not([disabled]):not([tabindex="-1"])',
+  'button:not([disabled]):not([tabindex="-1"])',
+  '[href]:not([tabindex="-1"])',
+  '[tabindex]:not([tabindex="-1"])',
+];
+
+export function isVisible(element: HTMLElement) {
+  return !!(
+    element.offsetWidth ||
+    element.offsetHeight ||
+    element.getClientRects().length
+  );
+}
+
+export function isTabKeyPressed(event: KeyboardEvent) {
+  return event.key === "Tab";
+}
+
+export function getFocusableElements(
+  trappedElement: HTMLElement
+): HTMLElement[] {
+  const focusableElements = Array.prototype.slice.call(
+    trappedElement.querySelectorAll(FOCUSABLE_ELEMENTS.join())
+  );
+  const visibleFocusableElements = focusableElements.filter(isVisible);
+
+  if (visibleFocusableElements.length === 0) {
+    throw new TypeError(
+      "There are no focusable elements in the trapped element provided"
+    );
+  }
+
+  return visibleFocusableElements;
+}
+
+export function getKeyDownEventHandler(
+  firstElement: HTMLElement,
+  lastElement: HTMLElement
+) {
+  return function (event: KeyboardEvent) {
+    if (!isTabKeyPressed(event)) {
+      return;
+    }
+
+    if (event.shiftKey && document.activeElement === firstElement) {
+      event.preventDefault();
+      lastElement.focus();
+    } else if (!event.shiftKey && document.activeElement === lastElement) {
+      event.preventDefault();
+      firstElement.focus();
+    }
+  };
+}
+
+export function getDisengageFocusTrapCallback(
+  trappedElement: HTMLElement,
+  keyDownEventHandler: (event: KeyboardEvent) => void,
+  lastActiveElement: HTMLElement
+) {
+  return function () {
+    trappedElement.removeEventListener("keydown", keyDownEventHandler);
+
+    if (lastActiveElement) {
+      lastActiveElement.focus();
+    }
+  };
+}
