ccf02cf886632be175b35cce416d8427046c5683
tigerabrodi <tigerabrodi@gmail.com>
2021-09-29 20:46:11 +0200

    feat(firebase): add getWithRecipeSlug util function to retrieve the recipe via slug

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app'
+import { getAuth, User } from 'firebase/auth'
 import {
   Timestamp,
   collection,
@@ -9,9 +9,11 @@ import {
   where,
   getDocs,
   limit,
-} from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-import { UserProfile } from "./types";
+  doc,
+  getDoc,
+} from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
+import { Recipe, UserProfile } from './types'
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_APP_API_KEY,
@@ -20,52 +22,75 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_APP_API_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_APP_API_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_API_APP_ID,
-};
+}
 
-const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig)
 
-export const firebaseDb = getFirestore(firebaseApp);
+export const firebaseDb = getFirestore(firebaseApp)
 
 // Auth export
-export const auth = getAuth(firebaseApp);
+export const auth = getAuth(firebaseApp)
 
 // Firestore Utils
-export const fromMillis = Timestamp.fromMillis;
+export const fromMillis = Timestamp.fromMillis
 
 // Storage export
-export const storage = getStorage(firebaseApp);
+export const storage = getStorage(firebaseApp)
 
 export const getUserWithUsername = async (username: string) => {
   if (!username) {
-    return;
+    return
   }
 
   const userQuery = query(
-    collection(firebaseDb, "users"),
-    where("username", "==", username),
+    collection(firebaseDb, 'users'),
+    where('username', '==', username),
     limit(1)
-  );
+  )
 
-  const user = (await getDocs(userQuery)).docs[0].data() as UserProfile;
+  const user = (await getDocs(userQuery)).docs[0].data() as UserProfile
 
-  if (user.joined !== null) {
-    return {
-      ...user,
-      joined: (user.joined as Timestamp).toMillis(),
-    } as UserProfile;
+  return {
+    ...user,
+    joined: (user.joined as Timestamp).toMillis(),
   }
-};
+}
+
+export const getRecipeWithSlug = async (
+  slug: string,
+  options?: { userToGetRecipeFrom: User | null | undefined }
+) => {
+  if (!slug) {
+    return
+  }
+
+  const queryPath = options?.userToGetRecipeFrom
+    ? `users/${options.userToGetRecipeFrom.uid}/recipes/${slug}`
+    : `recipes/${slug}`
+
+  const docRef = doc(firebaseDb, queryPath)
+  const docSnap = await getDoc(docRef)
+
+  if (docSnap.exists()) {
+    const recipe = docSnap.data() as Recipe
+
+    return {
+      ...recipe,
+      createdAt: (recipe.createdAt as Timestamp).toMillis(),
+    } as Recipe
+  }
+}
 
 export const recipeToJSON = (
   recipeDocument: DocumentSnapshot<{
-    createdAt: Timestamp;
-    updatedAt: Timestamp;
+    createdAt: Timestamp
+    updatedAt: Timestamp
   }>
 ) => {
-  const data = recipeDocument.data();
+  const data = recipeDocument.data()
   return {
     ...data,
     // Firestore timestamp NOT serializable to JSON. Must convert to milliseconds
     createdAt: data?.createdAt.toMillis() || 0,
-  };
-};
+  }
+}
