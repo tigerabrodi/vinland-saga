97f4fc010f9c93fc9aa272deabd5f0aee204f75b
tigerabrodi <tigerabrodi@gmail.com>
2021-09-29 20:57:57 +0200

    feat(RecipeEdit): add upload recipe image functionality

--- lib/types.ts
+++ lib/types.ts
@@ -1,19 +1,33 @@
-import { FieldValue, Timestamp } from "@firebase/firestore";
+import { User } from '@firebase/auth'
+import { FieldValue, Timestamp } from '@firebase/firestore'
 
 export type UserData = {
-  username: string | null;
-};
+  username: string | null
+  user: User | null | undefined
+}
 
 export type UserProfile = {
-  username: string;
-  email: string;
-  fullname: string;
-  age: string;
-  work: string;
-  location: string;
-  bio: string;
-  avatarUrl: string;
-  clapCount: number;
-  recipeCount: number;
-  joined: FieldValue | Timestamp | number;
-};
+  username: string
+  email: string
+  fullname: string
+  age: string
+  work: string
+  location: string
+  bio: string
+  avatarUrl: string
+  clapCount: number
+  recipeCount: number
+  joined: FieldValue | Timestamp | number
+}
+
+export type Recipe = {
+  title: string
+  body: string
+  commentsCount: number
+  clapCount: number
+  username: string
+  createdAt: FieldValue | Timestamp | number
+  uid: string
+  imageUrl: string
+  slug: string
+}
--- pages/[username]/[slug]/edit.tsx
+++ pages/[username]/[slug]/edit.tsx
@@ -1,11 +1,11 @@
-import * as React from "react";
-import Link from "next/link";
-import type { NextPage } from "next";
-import PlaceholderImage4x from "../../../assets/placeholder-image4x.jpg";
-import PlaceholderImage3x from "../../../assets/placeholder-image3x.jpg";
-import PlaceholderImage2x from "../../../assets/placeholder-image2x.jpg";
-import Eye from "../../../assets/eye.svg";
-import Rocket from "../../../assets/rocket.svg";
+import * as React from 'react'
+import Link from 'next/link'
+import type { NextPage } from 'next'
+import PlaceholderImage4x from '../../../assets/placeholder-image4x.jpg'
+import PlaceholderImage3x from '../../../assets/placeholder-image3x.jpg'
+import PlaceholderImage2x from '../../../assets/placeholder-image2x.jpg'
+import Eye from '../../../assets/eye.svg'
+import Rocket from '../../../assets/rocket.svg'
 import {
   RecipeEditForm,
   Heading,
@@ -23,19 +23,111 @@ import {
   ButtonWrapper,
   ButtonSave,
   ButtonPreview,
-} from "./editStyles";
-import { useFormState } from "@hooks/useFormState";
+} from './editStyles'
+import { useFormState } from '@hooks/useFormState'
+import { Router as NextRouter, useRouter } from 'next/router'
+import { Recipe } from '@lib/types'
+import { useLoadingStore } from '@lib/store'
+import { auth, firebaseDb, getRecipeWithSlug } from '@lib/firebase'
+import { doc, setDoc } from '@firebase/firestore'
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from '@firebase/storage'
+import toast from 'react-hot-toast'
+import { FullPageSpinner } from '@components/Spinner'
+import { useUserContext } from '@lib/context'
+
+type Router = NextRouter & {
+  query: {
+    slug: string
+  }
+}
 
 const RecipeEdit: NextPage = () => {
   const {
     handleChange,
+    setFormState,
     formState: { title, body },
   } = useFormState({
-    title: "",
-    body: "",
-  });
+    title: '',
+    body: '',
+  })
 
-  const isButtonSaveDisabled = !body.length || title.length < 3;
+  const isButtonSaveDisabled = !body.length || title.length < 3
+
+  const [recipe, setRecipe] = React.useState<Recipe | null>(null)
+  const [recipeImage, setRecipeImage] = React.useState('')
+  const { setStatus } = useLoadingStore()
+  const { user } = useUserContext()
+
+  const {
+    query: { slug },
+  } = useRouter() as Router
+
+  React.useEffect(() => {
+    const setRecipeState = async () => {
+      setStatus('loading')
+      setRecipe(
+        (await getRecipeWithSlug(slug, { userToGetRecipeFrom: user })) as Recipe
+      )
+      setStatus('success')
+    }
+
+    if (recipe) {
+      setFormState({
+        title: recipe.title,
+        body: recipe.body,
+      })
+      setRecipeImage(recipe.imageUrl)
+    } else {
+      setRecipeState()
+    }
+  }, [recipe, setFormState, setStatus, slug, user])
+
+  const uploadFile = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = Array.from(event.target.files as FileList)[0]
+    const extension = file.type.split('/')[1]
+
+    const recipeRef = doc(
+      firebaseDb,
+      `users/${auth.currentUser?.uid}/recipes/${slug}`
+    )
+
+    const storage = getStorage()
+    const recipeImageRef = ref(storage, `recipes/${slug}.${extension}`)
+
+    setStatus('loading')
+
+    const uploadTask = uploadBytesResumable(recipeImageRef, file)
+    uploadTask.on(
+      'state_changed',
+      () => {},
+      () => {
+        toast.error('Recipe image upload failed.')
+        setStatus('error')
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setDoc(recipeRef, { imageUrl: downloadURL }, { merge: true })
+          setRecipeImage(downloadURL)
+          setStatus('success')
+          toast.success('Successfully uploaded your recipe image.')
+        })
+      }
+    )
+  }
+
+  if (!recipe) {
+    return <FullPageSpinner />
+  }
+
+  const imageSrcSet =
+    recipeImage === ''
+      ? `${PlaceholderImage2x.src} 300w, ${PlaceholderImage3x.src} 768w, ${PlaceholderImage4x.src} 1280w`
+      : undefined
 
   return (
     <RecipeEditForm>
@@ -52,19 +144,20 @@ const RecipeEdit: NextPage = () => {
         />
       </TitleFormGroup>
       <Image
-        src={PlaceholderImage2x.src}
-        srcSet={`${PlaceholderImage2x.src} 300w, ${PlaceholderImage3x.src} 768w, ${PlaceholderImage4x.src} 1280w`}
-        alt="Placeholder"
+        src={recipeImage === '' ? PlaceholderImage2x.src : recipe.imageUrl}
+        srcSet={imageSrcSet}
+        alt={recipeImage === '' ? 'Placeholder' : recipe.title}
       />
       <FileInput
         type="file"
         id="upload"
         accept="image/x-png,image/gif,image/jpeg"
         aria-label="Upload Recipe Image"
+        onChange={uploadFile}
       />
       <UploadLabel htmlFor="upload">
-        {" "}
-        <ImageIcon />{" "}
+        {' '}
+        <ImageIcon />{' '}
       </UploadLabel>
       <BodyFormGroup>
         <Label htmlFor="body">Body</Label>
@@ -77,12 +170,12 @@ const RecipeEdit: NextPage = () => {
         />
       </BodyFormGroup>
       <Text>
-        The body uses{" "}
+        The body uses{' '}
         <Link passHref href="https://www.markdownguide.org/basic-syntax/">
           <TextLink target="_blank" rel="noopener noreferrer">
             Markdown.
           </TextLink>
-        </Link>{" "}
+        </Link>{' '}
         A simple and easy-to-use markup language.
       </Text>
 
@@ -96,7 +189,7 @@ const RecipeEdit: NextPage = () => {
         </ButtonPreview>
       </ButtonWrapper>
     </RecipeEditForm>
-  );
-};
+  )
+}
 
-export default RecipeEdit;
+export default RecipeEdit
