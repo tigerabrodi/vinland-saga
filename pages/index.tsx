91835e62399d2a0febb4388268a3044b486be5ec
tigerabrodi <tigerabrodi@gmail.com>
2021-10-07 06:58:46 +0200

    feat(RecipesFeed): add UI styles for mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,88 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next'
+import Link from 'next/link'
+import ClapSVG from '../assets/clap.svg'
+import BubbleSVG from '../assets/bubble.svg'
+import DummyRecipeImage from '../cypress/fixtures/recipe-image.jpg'
+import DummyAvatarImage from '../cypress/fixtures/tiger-avatar.png'
+import {
+  FeedSection,
+  TopWrapper,
+  Title,
+  ToolBar,
+  ToolBarButton,
+  RecipesList,
+  RecipeItem,
+  RecipeImage,
+  RecipeAvatar,
+  AuthorLink,
+  Date,
+  RecipeTitle,
+  ClapText,
+  CommentText,
+  ReadingTime,
+  RecipeTitleLink,
+} from './styles'
 
-const Home: NextPage = () => {
-  return <div>Hello World</div>;
-};
+const RecipesFeed: NextPage = () => {
+  return (
+    <FeedSection>
+      <TopWrapper>
+        <Title>Recipes</Title>
+        <ToolBar role="toolbar" aria-label="Sort by claps or newest date">
+          <ToolBarButton>Claps</ToolBarButton>
+          <ToolBarButton>Newest</ToolBarButton>
+        </ToolBar>
+      </TopWrapper>
+      <RecipesList>
+        <RecipeItem aria-label="Read the recipe Chicken Tikka">
+          <RecipeImage src={DummyRecipeImage.src} alt="Chicken Tikka" />
+          <RecipeAvatar src={DummyAvatarImage.src} alt="Tiger Abrodi" />
+          <Link passHref href="/">
+            <AuthorLink aria-label="Author: Tiger Abrodi">
+              Tiger Abrodi
+            </AuthorLink>
+          </Link>
+          <Date aria-label="Posted in 2021-09-09">2021-09-09</Date>
+          <RecipeTitle>
+            <Link passHref href="/">
+              <RecipeTitleLink>Chicken Tikka</RecipeTitleLink>
+            </Link>
+          </RecipeTitle>
+          <ClapText>
+            <ClapSVG />
+            12
+          </ClapText>
+          <CommentText>
+            <BubbleSVG />8
+          </CommentText>
+          <ReadingTime>4 min read</ReadingTime>
+        </RecipeItem>
+        <RecipeItem aria-label="Read the recipe Chicken Tikka">
+          <RecipeImage src={DummyRecipeImage.src} alt="Chicken Tikka" />
+          <RecipeAvatar src={DummyAvatarImage.src} alt="Tiger Abrodi" />
+          <Link passHref href="/">
+            <AuthorLink aria-label="Author: Tiger Abrodi">
+              Tiger Abrodi
+            </AuthorLink>
+          </Link>
+          <Date aria-label="Posted in 2021-09-09">2021-09-09</Date>
+          <RecipeTitle>
+            <Link passHref href="/">
+              <RecipeTitleLink>Chicken Tikka</RecipeTitleLink>
+            </Link>
+          </RecipeTitle>
+          <ClapText>
+            <ClapSVG />
+            12
+          </ClapText>
+          <CommentText>
+            <BubbleSVG />8
+          </CommentText>
+          <ReadingTime>4 min read</ReadingTime>
+        </RecipeItem>
+      </RecipesList>
+    </FeedSection>
+  )
+}
 
-export default Home;
+export default RecipesFeed
---
+++ pages/styles.ts
@@ -0,0 +1,152 @@
+import {
+  pinkFocusStyles,
+  sectionStyles,
+  whiteFocusStyles,
+  yellowFocusStyles,
+} from '@styles/sharedStyles'
+import { theme } from '@styles/theme'
+import styled, { css } from 'styled-components'
+
+export const FeedSection = styled.div`
+  ${sectionStyles}
+  justify-content: flex-start;
+  width: 28rem;
+  margin-bottom: 2rem;
+`
+
+export const TopWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin-top: 3rem;
+`
+
+export const Title = styled.h1`
+  font-weight: bold;
+  font-size: 2.5rem;
+  color: ${theme.Pink};
+`
+
+export const ToolBar = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  column-gap: 2rem;
+`
+
+export const ToolBarButton = styled.button`
+  color: white;
+  font-weight: bold;
+  font-size: 1.4rem;
+  ${whiteFocusStyles}
+`
+
+export const RecipesList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  row-gap: 3rem;
+  margin-top: 2rem;
+`
+
+export const RecipeItem = styled.li`
+  width: 28rem;
+  height: 28.5rem;
+  display: grid;
+  grid-template-areas:
+    'image image image image'
+    'avatar author .... ....'
+    'avatar date ..... .....'
+    'title title ..... .....'
+    'clap comment .... time';
+  align-items: center;
+  justify-items: center;
+  border: 0.2rem solid ${theme.Yellow};
+  border-radius: 0.2rem;
+  box-shadow: 0 0.2rem 0.2rem black;
+  grid-template-columns: 30% 30% 10% 30%;
+  grid-template-rows: 55% auto auto auto 3.8rem;
+  overflow: hidden;
+`
+
+export const RecipeImage = styled.img`
+  grid-area: image;
+  width: 100%;
+  height: auto;
+`
+
+export const RecipeAvatar = styled.img`
+  grid-area: avatar;
+  height: 4rem;
+  width: 4rem;
+  border-radius: 50%;
+  position: relative;
+  bottom: -0.4rem;
+`
+
+export const AuthorLink = styled.a`
+  grid-area: author;
+  font-weight: 500;
+  font-size: 1.2rem;
+  text-decoration: underline;
+  color: ${theme.Pink};
+  align-self: self-end;
+  justify-self: self-start;
+  ${pinkFocusStyles}
+`
+
+export const Date = styled.p`
+  font-weight: 500;
+  font-size: 1rem;
+  color: ${theme.Pink};
+  grid-area: date;
+  justify-self: self-start;
+`
+
+export const RecipeTitle = styled.h2`
+  align-self: self-end;
+  grid-area: title;
+`
+
+export const RecipeTitleLink = styled.a`
+  font-weight: bold;
+  font-size: 2rem;
+  color: ${theme.Yellow};
+  text-decoration: underline;
+  padding-left: 1.6rem;
+  ${yellowFocusStyles}
+`
+
+const textStyles = css`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  font-weight: 400;
+  column-gap: 0.4rem;
+  font-size: 1.3rem;
+  color: ${theme.Pink};
+  svg {
+    height: 2rem;
+    width: 2rem;
+  }
+`
+
+export const ClapText = styled.p`
+  ${textStyles}
+  grid-area: clap;
+`
+
+export const CommentText = styled.p`
+  ${textStyles}
+  grid-area: comment;
+`
+
+export const ReadingTime = styled.p`
+  grid-area: time;
+  color: ${theme.Yellow};
+  font-size: 1.2rem;
+  font-weight: 400;
+  margin-right: 1rem;
+`
