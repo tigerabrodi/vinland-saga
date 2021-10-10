d55513259b8b3b76af2f316837b66353c1373c56
tigerabrodi <tigerabrodi@gmail.com>
2021-10-10 17:58:01 +0200

    fix(useUserData): add the different status states

--- hooks/auth/useUserData.ts
+++ hooks/auth/useUserData.ts
@@ -3,6 +3,7 @@ import { auth, firebaseDb } from '@lib/firebase'
 import { useEffect, useState } from 'react'
 import { useAuthState } from '@hooks/auth/useAuthState'
 import { UserData } from '@lib/types'
+import { useLoadingStore } from '@lib/store'
 
 type User = {
   username: string
@@ -10,23 +11,27 @@ type User = {
 
 export function useUserData(): UserData {
   const { user } = useAuthState(auth)
+  const { setStatus } = useLoadingStore()
   const [username, setUsername] = useState<string | null>(null)
 
   useEffect(() => {
     let unsubscribe
+    setStatus('loading')
 
     if (user) {
       unsubscribe = onSnapshot(doc(firebaseDb, 'users', user.uid), (doc) => {
         if (doc.exists()) {
           setUsername((doc.data() as User).username)
+          setStatus('success')
         }
       })
     } else {
       setUsername(null)
+      setStatus('error')
     }
 
     return unsubscribe
-  }, [user])
+  }, [setStatus, user, username])
 
   return { username, user }
 }
