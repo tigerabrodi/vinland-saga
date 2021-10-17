95da846ef3f7e0992d0d54c13099e28fffece4dd
tigerabrodi <tigerabrodi@gmail.com>
2021-10-17 15:05:12 +0200

    feat(NewRecipeModal): use new modal component within component

--- components/NewRecipeModal/index.tsx
+++ components/NewRecipeModal/index.tsx
@@ -1,29 +1,17 @@
 import * as React from 'react'
 import { doc, serverTimestamp, setDoc } from '@firebase/firestore'
-import { useFocusTrap } from '@hooks/useFocusTrap'
 import { useFormState } from '@hooks/useFormState'
 import { v4 as uuidv4 } from 'uuid'
 import kebabCase from 'lodash.kebabcase'
 import { useLoadingStore, useNewRecipeStore } from '@lib/store'
-import {
-  Modal,
-  ModalBackground,
-  Form,
-  Title,
-  CreateButton,
-  Close,
-  CloseButton,
-  Label,
-  Input,
-} from './styles'
-import { useCloseEscape } from '@hooks/useCloseEscape'
-import { useClickOutside } from '@hooks/useClickOutside'
+import { Form, CreateButton, Label, Input } from './styles'
 import { useUserContext } from '@lib/context'
 import { firebaseDb } from '@lib/firebase'
 import { useRouter } from 'next/router'
 import toast from 'react-hot-toast'
 import { Recipe } from '@lib/types'
 import { useGetUser } from '@hooks/auth/useGetUser'
+import { Modal } from '@components/Modal'
 
 export const NewRecipeModal = () => {
   const {
@@ -34,16 +22,6 @@ export const NewRecipeModal = () => {
   const { setIsModalOpen, isModalOpen } = useNewRecipeStore()
 
   const { setStatus } = useLoadingStore()
-
-  const modalRef = useFocusTrap(isModalOpen)
-
-  useClickOutside({
-    ref: modalRef,
-    callback: () => setIsModalOpen(false),
-    shouldTriggerCallback: isModalOpen,
-  })
-
-  useCloseEscape(() => setIsModalOpen(false))
 
   const { push } = useRouter()
 
@@ -94,37 +72,25 @@ export const NewRecipeModal = () => {
     }
   }
 
-  return isModalOpen ? (
-    <>
-      <Modal
-        role="dialog"
-        aria-modal="true"
-        aria-labelledby="modalTitle"
-        ref={modalRef}
-      >
-        <Title id="modalTitle">Create Recipe</Title>
-        <CloseButton
-          type="button"
-          onClick={() => setIsModalOpen(false)}
-          aria-label="Close"
-        >
-          <Close />
-        </CloseButton>
-        <Form onSubmit={handleSubmit}>
-          <Label htmlFor="title">Title</Label>
-          <Input
-            placeholder="Chicken Tikka"
-            name="title"
-            id="title"
-            onChange={handleChange}
-            value={title}
-          />
-          <CreateButton type="submit" aria-disabled={title.length < 3}>
-            Create
-          </CreateButton>
-        </Form>
-      </Modal>
-      <ModalBackground />
-    </>
-  ) : null
+  return (
+    <Modal
+      isOpen={isModalOpen}
+      setIsOpen={setIsModalOpen}
+      title="Create Recipe"
+    >
+      <Form onSubmit={handleSubmit}>
+        <Label htmlFor="title">Title</Label>
+        <Input
+          placeholder="Chicken Tikka"
+          name="title"
+          id="title"
+          onChange={handleChange}
+          value={title}
+        />
+        <CreateButton type="submit" aria-disabled={title.length < 3}>
+          Create
+        </CreateButton>
+      </Form>
+    </Modal>
+  )
 }
