2ef0c1af241242922fb29cf6f3251762657cf9e9
tigerabrodi <tigerabrodi@gmail.com>
2021-10-21 06:01:29 +0200

    feat(RecipesFeedHome): use feed component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,9 @@
+import { Feed } from '@components/Feed'
 import { RecipesFeed } from '@components/RecipesFeed'
 import { collectionGroup, getDocs, query } from '@firebase/firestore'
 import { firebaseDb, recipeToJSON } from '@lib/firebase'
 import { Recipe } from '@lib/types'
 import type { NextPage } from 'next'
-import {
-  FeedSection,
-  TopWrapper,
-  Title,
-  ToolBar,
-  ToolBarButton,
-  NoFoundText,
-} from './styles'
 
 // TODO use this to limit the the queries and add a load more button.
 // const MAX_RECIPES_PER_PAGE = 10
@@ -33,20 +26,13 @@ type Props = {
 
 const RecipesFeedHome: NextPage<Props> = ({ recipes }) => {
   return (
-    <FeedSection>
-      <TopWrapper>
-        <Title>Recipes</Title>
-        <ToolBar role="toolbar" aria-label="Sort by claps or newest date">
-          <ToolBarButton>Claps</ToolBarButton>
-          <ToolBarButton>Newest</ToolBarButton>
-        </ToolBar>
-      </TopWrapper>
-      {recipes.length ? (
-        <RecipesFeed recipes={recipes} />
-      ) : (
-        <NoFoundText>Currently no recipes exist.</NoFoundText>
-      )}
-    </FeedSection>
+    <Feed
+      toolbarButtonLabels={['Claps', 'Newest']}
+      title="Recipes"
+      itemsLength={recipes.length}
+    >
+      <RecipesFeed recipes={recipes} />
+    </Feed>
   )
 }
 
