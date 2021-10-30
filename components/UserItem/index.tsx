6f32ae3dd299e0bca78cf4b05d5ab4f90c1e9fa6
tigerabrodi <tigerabrodi@gmail.com>
2021-10-30 06:24:23 +0200

    feat(UserItem): add component

---
+++ components/UserItem/index.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link'
+import RecipeSVG from '../../assets/recipe.svg'
+import defaultAvatar from '../../assets/default-avatar.png'
+import ClapSVG from '../../assets/clap.svg'
+import {
+  ListItem,
+  AvatarImage,
+  Fullname,
+  Username,
+  Location,
+  RecipesCount,
+  ClapsCount,
+} from './styles'
+import { UserProfile } from '@lib/types'
+
+type Props = {
+  user: UserProfile
+}
+
+export const UserItem = ({
+  user: { username, fullname, location, recipeCount, clapCount, avatarUrl },
+}: Props) => (
+  <ListItem>
+    <AvatarImage src={avatarUrl === '' ? defaultAvatar.src : avatarUrl} />
+    <Fullname>
+      <Link href={`/${username}`} passHref>
+        {fullname}
+      </Link>
+    </Fullname>
+    <Username>@{username}</Username>
+    <Location>{location}</Location>
+    <RecipesCount aria-label={`${recipeCount} recipes`}>
+      <RecipeSVG />
+      <span>12</span>
+    </RecipesCount>
+    <ClapsCount aria-label={`${clapCount} claps`}>
+      <ClapSVG />
+      <span>12</span>
+    </ClapsCount>
+  </ListItem>
+)
