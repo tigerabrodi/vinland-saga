622fb1538318e190a86f405a6046238755926b51
tigerabrodi <tigerabrodi@gmail.com>
2021-11-05 18:00:06 +0100

    feat(users): add handling of sorting users

--- pages/users.tsx
+++ pages/users.tsx
@@ -1,9 +1,11 @@
+import * as React from 'react'
 import { Feed } from '@components/Feed'
 import { UserItem } from '@components/UserItem'
 import {
   collection,
   CollectionReference,
   getDocs,
+  orderBy,
   query,
 } from '@firebase/firestore'
 import { firebaseDb } from '@lib/firebase/firebase'
@@ -11,29 +13,65 @@ import { dataToJSON } from '@lib/firebase/format-utils'
 import { UserProfile } from '@lib/types'
 import type { NextPage } from 'next'
 import { List } from './usersStyles'
+import { useLoadingStore } from '@lib/store'
 
 export async function getServerSideProps() {
   const usersQuery = query(
-    collection(firebaseDb, 'users') as CollectionReference<UserProfile>
+    collection(firebaseDb, 'users') as CollectionReference<UserProfile>,
+    orderBy('clapCount', 'desc')
   )
 
   const users = (await getDocs(usersQuery)).docs.map(dataToJSON)
 
   return {
-    props: { users },
+    props: { ssrUsers: users },
   }
 }
 
 type Props = {
-  users: UserProfile[]
+  ssrUsers: UserProfile[]
 }
 
-const UsersFeed: NextPage<Props> = ({ users }) => {
+const UsersFeed: NextPage<Props> = ({ ssrUsers }) => {
+  const [sortedUsers, setSortedUsers] = React.useState<UserProfile[] | null>(
+    null
+  )
+  const [sortingValue, setSortingValue] = React.useState('')
+  const { setStatus } = useLoadingStore()
+
+  const users = sortedUsers || ssrUsers
+
+  React.useEffect(() => {
+    if (sortingValue === '') {
+      return
+    }
+
+    const setUsers = async () => {
+      setStatus('loading')
+      const usersQuery = query<UserProfile>(
+        collection(firebaseDb, 'users') as CollectionReference<UserProfile>,
+        sortingValue === 'Claps'
+          ? orderBy('clapCount', 'desc')
+          : orderBy('recipeCount', 'desc')
+      )
+
+      const users = (await getDocs<UserProfile>(usersQuery)).docs.map(
+        dataToJSON
+      )
+
+      setSortedUsers(users)
+      setStatus('success')
+    }
+
+    setUsers()
+  }, [setStatus, sortingValue])
+
   return (
     <Feed
-      labels={['Hearts', 'Recipes']}
+      labels={['Claps', 'Recipes']}
       title="Users"
       itemsLength={users.length}
+      setSortingValue={setSortingValue}
     >
       <List>
         {users.map((user) => (
