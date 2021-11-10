4eb98181c24b2c7a61ad02a296ca1d36c00d1ba1
tigerabrodi <tigerabrodi@gmail.com>
2021-11-10 19:17:37 +0100

    refactor(icons): unique string to id, more explicit

--- icons/Close.tsx
+++ icons/Close.tsx
@@ -1,18 +1,18 @@
 import { v4 as uuidv4 } from 'uuid'
 
 export const CloseIcon = () => {
-  const uniqueString = uuidv4()
+  const uniqueId = uuidv4()
 
   return (
     <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18">
-      <g clipPath={`url(#${uniqueString})`}>
+      <g clipPath={`url(#${uniqueId})`}>
         <path
           d="M18 15.141l-6.236-6.157 6.15-6.211L15.14 0 8.982 6.239 2.75.086 0 2.836l6.24 6.18L.087 15.25 2.836 18l6.178-6.239 6.213 6.153L18 15.14z"
           fill="#F4A7A3"
         />
       </g>
       <defs>
-        <clipPath id={uniqueString}>
+        <clipPath id={uniqueId}>
           <path fill="#fff" d="M0 0h18v18H0z" />
         </clipPath>
       </defs>
--- icons/Eye.tsx
+++ icons/Eye.tsx
@@ -1,18 +1,18 @@
 import { v4 as uuidv4 } from 'uuid'
 
 export const EyeIcon = () => {
-  const uniqueString = uuidv4()
+  const uniqueId = uuidv4()
 
   return (
     <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11 11">
-      <g clipPath={`url(#${uniqueString})`}>
+      <g clipPath={`url(#${uniqueId})`}>
         <path
           d="M5.5 2.52c-1.91 0-3.643 1.045-4.922 2.743a.396.396 0 0 0 0 .472C1.857 7.435 3.59 8.48 5.5 8.48c1.91 0 3.643-1.045 4.922-2.743a.396.396 0 0 0 0-.472C9.143 3.565 7.41 2.52 5.5 2.52Zm.137 5.079a2.105 2.105 0 0 1-2.236-2.236A2.108 2.108 0 0 1 5.363 3.4a2.105 2.105 0 0 1 2.236 2.236A2.115 2.115 0 0 1 5.637 7.6Zm-.063-.97A1.13 1.13 0 0 1 4.37 5.426 1.137 1.137 0 0 1 5.428 4.37a1.13 1.13 0 0 1 1.203 1.203 1.137 1.137 0 0 1-1.057 1.057Z"
           fill="#EFD582"
         />
       </g>
       <defs>
-        <clipPath id={uniqueString}>
+        <clipPath id={uniqueId}>
           <path fill="#fff" transform="translate(.5 .5)" d="M0 0h10v10H0z" />
         </clipPath>
       </defs>
